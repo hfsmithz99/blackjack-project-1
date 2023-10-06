f1abca32f770c2a8b169a86705302807166ae893
Henry Smith <h.f.smithz99@gmail.com>
2023-10-06 14:25:44 -0400

    updating pseudocode

---
+++ js/app.js
@@ -0,0 +1,50 @@
+//--------Constant Variables----------
+
+//set Constant variables
+//Number of cars in deck
+
+//--------State Variables-------------
+//cards in hand
+let handCards;
+//wins and losses of hands
+let wins;
+let losses;
+//players money pot
+let playerBank;
+//amount for bet
+let amountBet;
+
+//--------Cached DOM Elements---------
+//cache the buttons
+    //play again
+const playAgainBtn =
+    //bet 1 or 5
+const betOneBtn
+const betFiveBtn
+    //stand
+const standBtn
+    //hit
+const hitBtn
+//cache the elements of DOM that need to be updated by render()
+    //amount bet
+    //cards in hand
+
+
+//--------Initialize the Game---------
+init();
+//--------Functions-------------------
+function init() {
+
+}
+
+
+//write function for if the player hits or stands
+//render function
+    //will need to update the cards on the table and the amount bet
+
+//--------Code Explanation-----------
+
+//1.
+//2.
+//3.
+//4.
